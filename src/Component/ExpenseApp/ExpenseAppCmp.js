8d6c7f105ad857e98a8937c12d31f93393117456
AliReza <alirezaesm921@gmail.com>
2022-07-13 10:08:57 +0430

    first Commit-add TaiwlindCss

---
+++ src/Component/ExpenseApp/ExpenseAppCmp.js
@@ -0,0 +1,9 @@
+const ExpenseApp = () => {
+    return ( 
+        <section className=" bg-green-800 cursor-wait mt-8 md:mt-16 "> 
+            ExpenseApp 
+        </section>
+     );
+}
+ 
+export default ExpenseApp;
