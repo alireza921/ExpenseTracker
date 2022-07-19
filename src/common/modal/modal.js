1f7cfb4f390013f8c1207036f6639c914ea33da6
AliReza <alirezaesm921@gmail.com>
2022-07-19 09:50:52 +0430

    transAction=delete button added)

--- src/common/modal/modal.js
+++ src/common/modal/modal.js
@@ -31,7 +31,7 @@ const Modal = () => {
       </div>
       <div
         className={`${
-          isShow ? " -translate-y-80 h-full " : " hidden  "
+          isShow ? " -translate-y-80 h-full " : " opacity-0  "
         } ${" mt-5 z-10 p-2  duration-500 bg-violet-200 fixed top-96 left-0 w-full rounded-t-2xl  "}`}>
         <header className="flex justify-around m-2 p-2   ">
           <button className="bg-red-400 text-blue-800  font-medium  p-2     rounded-lg  "  onClick={expenseHandler}> expense </button>
--- src/pages/transAction/transAction.js
+++ src/pages/transAction/transAction.js
@@ -1,21 +1,54 @@
-import { useLocation, useParams } from "react-router-dom";
-import { useTransActions } from "../../context/transAction/transActionsContextProvider";
+import { useLocation, useNavigate } from "react-router-dom";
+import {
+  useTransActions,
+  useTransActionsAction,
+} from "../../context/transAction/transActionsContextProvider";
 
 const TransAction = () => {
   const location = useLocation();
-  console.log(location.state);
   const transAction = location.state;
-  const params = useParams();
-  console.log(params.id);
 
+  const navigate = useNavigate();
+
+  const transActions = useTransActions();
+  const setTransAction = useTransActionsAction();
+
+  const deleteHandler = (id) => {
+    console.log(id);
+    const filteredTransAction = transActions.filter(
+      (transAction) => transAction.id !== id
+    );
+    setTransAction(filteredTransAction);
+    navigate("/");
+  };
+  const editHandler = (id) => {
+    console.log(id);
+  };
   return (
-    <ul className={`${transAction.type === 'expense' ? 'border-red-400' : 'border-green-400'} ${'flex flex-col  items-strat  justify-center  bg-violet-100  text-blue-800 font-semibold border border-solid rounded-xl py-4 px-1 my-3'} `}  >
-      <li className='m-1'> Date : {transAction.date} </li>
-      <li className='m-1'> Title : {transAction.title} </li>
-      <li className='m-1'> Amount : {transAction.amount} </li>
-      <li className='m-1'> Type : {transAction.type} </li>
-      <li className='m-1'> Describtion : {transAction.desc} </li>
-    </ul>
+    <section
+      className={`${
+        transAction.type === "expense" ? "border-red-400" : "border-green-400"
+      } ${"flex - justify-between  bg-violet-100  text-blue-800 font-semibold border border-solid rounded-xl py-4 px-1 my-3"}`}>
+      <ul className='flex flex-col  items-strat  justify-center '>
+        <li className='m-1'> Date : {transAction.date} </li>
+        <li className='m-1'> Title : {transAction.title} </li>
+        <li className='m-1'> Amount : {transAction.amount} </li>
+        <li className='m-1'> Type : {transAction.type} </li>
+        <li className='m-1'> Describtion : {transAction.desc} </li>
+      </ul>
+      <article className='flex flex-col ml-4 p-4 '>
+        <button
+          onClick={() => deleteHandler(transAction.id)}
+          className='px-4 py-2 bg-violet-800 text-violet-200  my-2 rounded-lg'>
+          delete
+        </button>
+        <button
+          onClick={() => editHandler(transAction.id)}
+          className='px-4 py-2 bg-violet-800 text-violet-200  my-2 rounded-lg'>
+          Edit
+        </button>
+      </article>
+    </section>
   );
 };
 
