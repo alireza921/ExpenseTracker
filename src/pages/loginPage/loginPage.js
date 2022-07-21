5f8670f39364bc2fe6d25c42b894b01a46072431
AliReza <alirezaesm921@gmail.com>
2022-07-21 14:39:28 +0430

    add Formik Library to LoginPage

--- src/pages/loginPage/loginPage.js
+++ src/pages/loginPage/loginPage.js
@@ -1,53 +1,77 @@
 import { useState } from "react";
 import { useAuth, useAuthAction } from "../../context/auth/auhContextProvider";
 import { useNavigate } from "react-router-dom";
+import { useFormik } from "formik";
+import * as yup from "yup";
+const initialValues = {
+  name: "",
+  email: "",
+};
+
+const validationSchema = yup.object({
+  name: yup
+    .string()
+    .required("Name is Required !")
+    .min(6, "Minimum 6 charachter "),
+  email: yup.string().email("Email Is Invalid !").required("Email is Required"),
+});
 
 const LoginPage = () => {
-  const navigate = useNavigate() ;
-  const auth = useAuth();
+  const navigate = useNavigate();
   const setAuth = useAuthAction();
-  const [login, setLogin] = useState({
-    name: "",
-    email: "",
-  });
-  const changeHandler = (e) => {
-    console.log(e.target.value);
-    setLogin({ ...login, [e.target.name]: e.target.value });
+
+  const onSubmit = (values, { resetForm }) => {
+    setAuth(values);
+    console.log("values");
+    resetForm({ values: "" });
+    navigate("/");
   };
 
-  const submitFormHandler = (e) => {
-    e.preventDefault();
-    setAuth(login);
-    console.log(auth);
-    setLogin({
-      name : "" , email: ""
-    })
-    navigate('/')
-  };
+  const formik = useFormik({
+    initialValues,
+    onSubmit,
+    validationSchema,
+    validateOnMount: true,
+  });
 
   return (
-    <form onSubmit={submitFormHandler}>
-      <div className="my-4">
-        <label className="font-semibold "> Name : </label>
+    <form onSubmit={formik.handleSubmit}>
+      <div className='my-4'>
+        <label className='font-semibold '> Name : </label>
         <input
           type='text'
-          onChange={changeHandler}
-          value={login.name}
+          onChange={formik.handleChange}
+          onBlur={formik.handleBlur}
+          value={formik.values.name}
           name='name'
           className='border-2 border-violet-400 rounded-lg px-1 outline-none w-full my-1 '
         />
+        {formik.errors.name && formik.touched.name && (
+          <div className='text-red-600 mb-1'> {formik.errors.name} </div>
+        )}
       </div>
-      <div >
-        <label className="font-semibold " > Email : </label>
+      <div>
+        <label className='font-semibold '> Email : </label>
         <input
           type='email'
-          onChange={changeHandler}
-          value={login.email}
+          onChange={formik.handleChange}
+          onBlur={formik.handleBlur}
+          value={formik.values.email}
           name='email'
           className='border-2 border-violet-400 rounded-lg px-1 outline-none w-full my-1'
         />
+        {formik.errors.email && formik.touched.email && (
+          <div className='text-red-600 mb-1'> {formik.errors.email} </div>
+        )}
       </div>
-      <button className=" border-solid w-full bg-violet-400 p-2 text-indigo-50 my-2 rounded-xl"> submit </button>
+      <button
+        disabled={!formik.isValid}
+        type='submit'
+        className={`${
+          !formik.isValid && "bg-gray-200 text-violet-800 cursor-not-allowed"
+        } ${" border-solid w-full bg-violet-400 p-2 text-indigo-50 my-2 rounded-xl"}`}>
+        submit
+      </button>
     </form>
   );
 };
